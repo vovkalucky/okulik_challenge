3a00c3567280877a9e754c755189955de8475337
vladimir <vladimir.inves@gmail.com>
2024-08-30 22:40:34 +0300

    add .gitignore + small fix

--- conftest.py
+++ conftest.py
@@ -3,12 +3,13 @@ from playwright.sync_api import sync_playwright
 
 
 # Параметризуем фикстуру для использования всех браузеров
-@pytest.fixture(params=["chromium", "firefox", "webkit"]) #, "firefox", "webkit"
+@pytest.fixture(params=["chromium"]) #, "firefox", "webkit"
 def browser(request):
     with sync_playwright() as p:
-        browser = p[request.param].launch()
+        browser = p[request.param].launch(headless=False)
         yield browser
         browser.close()
+
 
 @pytest.fixture
 def page(browser):
@@ -17,3 +18,4 @@ def page(browser):
     page.set_viewport_size({'height': 1080, 'width': 1920})
     yield page
     context.close()
+
